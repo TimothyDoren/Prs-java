1b992df0f73f11aa428b0b1ef65240ebd6634aa6
Tim Doren <dorentim@gmail.com>
2023-03-30 11:48:55 -0400

    finished requestController and all the required methods

--- src/main/java/com/maxtrain/bootcamp/prs/request/RequestController.java
+++ src/main/java/com/maxtrain/bootcamp/prs/request/RequestController.java
@@ -1,7 +1,9 @@
 package com.maxtrain.bootcamp.prs.request;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
+import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.maxtrain.bootcamp.prs.user.User;
+import com.maxtrain.bootcamp.prs.user.UserRepository;
 
 import jakarta.persistence.*;
 
@@ -22,9 +26,16 @@ import jakarta.persistence.*;
 @RestController
 @RequestMapping("/api/requests")
 public class RequestController {
+	private final String Status_New = "NEW";
+	private final String Status_Review = "REVIEW";
+	private final String Status_Approved = "APPROVED";
+	private final String Status_Rejected = "REJECTED";
 
 		@Autowired
 		private RequestRepository requestRepo;
+		
+		@Autowired
+		private UserRepository userRepo;
 		
 		@GetMapping
 		public ResponseEntity<Iterable<Request>> GetRequests(){
@@ -38,6 +49,26 @@ public class RequestController {
 				return new ResponseEntity(HttpStatus.NOT_FOUND);
 			}
 			return new ResponseEntity<Request>(request.get(), HttpStatus.OK);
+		}
+		@GetMapping("reviews/{userId}")
+		public ResponseEntity<Iterable<Request>> GetReviews(@PathVariable int userId){
+			//retrieve user and check if empty
+			Optional<User> user = userRepo.findById(userId);
+			if(user.isEmpty() || user.get().getId() != userId) {
+				return new ResponseEntity(HttpStatus.BAD_REQUEST);
+			}
+			//retrieve requests with status of review
+			Iterable<Request> requests = requestRepo.findByStatus(Status_Review);
+			//make a list that stores these requests so it can be altered in loop
+			ArrayList<Request> reviewRequests = new ArrayList<Request>();
+			for(var r : requests) {
+				if(r.getUser().getId() != userId) {
+				//add the request to list if the userId does not match
+				reviewRequests.add(r);
+				}
+			}
+			//return new collection
+			return new ResponseEntity<Iterable<Request>>(reviewRequests, HttpStatus.OK);
 		}
 		@PostMapping
 		public ResponseEntity<Request> postRequest(@RequestBody Request request){
@@ -55,14 +86,36 @@ public class RequestController {
 		}
 		@SuppressWarnings("rawtypes")
 		@PutMapping("review/{id}")
-		public ResponseEntity approveRequest(@PathVariable int id, @RequestBody Request request) {
+		public ResponseEntity reviewRequest(@PathVariable int id, @RequestBody Request request) {
 			if(request.getId() != id) {
 				return new ResponseEntity(HttpStatus.BAD_REQUEST);
 			}
 			if(request.getTotal() <= 50) {
 				request.setStatus("APPROVED");
 			}
-			request.setStatus("REVIEW");
+			else if(request.getTotal() > 50) {
+				request.setStatus("REVIEW");
+			}
+			requestRepo.save(request);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+		@SuppressWarnings("rawtypes")
+		@PutMapping("approve/{id}")
+		public ResponseEntity approveRequest(@PathVariable int id, @RequestBody Request request) {
+			if(request.getId() != id) {
+				return new ResponseEntity(HttpStatus.BAD_REQUEST);
+			}
+			request.setStatus("APPROVED");
+			requestRepo.save(request);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+		@SuppressWarnings("rawtypes")
+		@PutMapping("reject/{id}")
+		public ResponseEntity rejectRequest(@PathVariable int id, @RequestBody Request request) {
+			if(request.getId() != id) {
+				return new ResponseEntity(HttpStatus.BAD_REQUEST);
+			}
+			request.setStatus("REJECTED");
 			requestRepo.save(request);
 			return new ResponseEntity(HttpStatus.NO_CONTENT);
 		}
--- src/main/java/com/maxtrain/bootcamp/prs/request/RequestRepository.java
+++ src/main/java/com/maxtrain/bootcamp/prs/request/RequestRepository.java
@@ -3,5 +3,5 @@ package com.maxtrain.bootcamp.prs.request;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RequestRepository extends CrudRepository<Request, Integer> {
-
+	Iterable<Request> findByStatus(String status);
 }
